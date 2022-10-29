21e271679626919ecbfc5d2084f00410455b6e9f
Ankit Bajpai <ankitbajpaib070@gmail.com>
2022-10-29 14:59:17 +0530

    Current typescript-sdk-odc works just with one drive personal. I have update code to support both one drive personal and one drive sharepoint. Based on oneDriveAccountType set by dev, either OD or ODSP will be invoked

--- samples/file-picking/typescript-sdk-odc/src/main.ts
+++ samples/file-picking/typescript-sdk-odc/src/main.ts
@@ -3,39 +3,32 @@ import {
     IFilePickerOptions,
     INotificationData,
     IPickData,
+    OneDriveConsumerInit,
+    ODSPInit,
+    IAuthenticateCommand,
 } from "@pnp/picker-sdk";
 
-import { getTokenWithScopes } from "./auth";
+import { getToken } from "./auth";
 
 document.onreadystatechange = () => {
     if (document.readyState === "interactive") {
         const button: HTMLButtonElement = <any>document.getElementById("launchpicker");
-        button.onclick = createWindow;
+        button.onclick = launchOneDrivePicker;
     }
 };
 
-const options: IFilePickerOptions = {
-    sdk: "8.0",
-    entry: {
-        oneDrive: {}
-    },
-    authentication: {},
-    messaging: {
-        origin: "http://localhost:3000",
-        channelId: "27"
-    },
-};
+const sharepoint_baseUrl = "https://tenant-my.sharepoint.com";
 
-async function createWindow(e) {
+async function launchOneDrivePicker(e) {
 
     e.preventDefault();
 
+    const accountType: "Consumer" | "ODSP" = "Consumer";
+
+    const init: OneDriveConsumerInit | ODSPInit = getOneDriveInit(accountType);
+
     // setup the picker with the desired behaviors
-    const picker = await Picker(window.open("", "Picker", "width=800,height=600"), {
-        type: "Consumer",
-        options,
-        tokenFactory: () => getTokenWithScopes(["OneDrive.ReadWrite"]),
-    });
+    const picker = await Picker(window.open("", "Picker", "width=800,height=600"), init);
 
     picker.addEventListener("pickernotifiation", (e: CustomEvent<INotificationData>) => {
 
@@ -47,3 +40,83 @@ async function createWindow(e) {
         document.getElementById("pickerResults").innerHTML = `<pre>${JSON.stringify(e.detail, null, 2)}</pre>`
     });
 }
+function getFilePickerOptions(accountType: string): IFilePickerOptions {
+    if (accountType == "Consumer") {
+        return {
+            sdk: "8.0",
+            entry: {
+                oneDrive: {}
+            },
+            authentication: {},
+            messaging: {
+                origin: "http://localhost:3000",
+                channelId: "27"
+            },
+        };
+
+    } else {
+        return {
+            sdk: "8.0",
+            entry: {
+                sharePoint: {
+                    byPath: {
+                        web: sharepoint_baseUrl,
+                        list: "SitePages"
+                    }
+                }
+            },
+            authentication: {},
+            messaging: {
+                origin: "http://localhost:3000",
+                channelId: "27"
+            },
+            typesAndSources: {
+                mode: "all",
+                filters: [".aspx"],
+                pivots: {
+                    recent: false,
+                    oneDrive: false
+                }
+            }
+        };
+    }
+}
+
+function getOneDriveInit(accountType: string): OneDriveConsumerInit | ODSPInit {
+
+    const options: IFilePickerOptions = getFilePickerOptions(accountType);
+    
+    let authCmd : IAuthenticateCommand = null;
+
+    if (accountType == "Consumer") {
+        authCmd = {
+            command: "authenticate",
+            type: "Graph",
+            resource: "",
+        }
+        const init: OneDriveConsumerInit = {
+            type: "Consumer",
+            options,
+            tokenFactory: () => getToken(authCmd),
+        };
+
+        return init;
+
+    } else {
+
+        authCmd = {
+            command: "authenticate",
+            type: "SharePoint",
+            resource: sharepoint_baseUrl,
+        }
+
+        const init: ODSPInit = {
+            type: "ODSP",
+            baseUrl: sharepoint_baseUrl,
+            options,
+            tokenFactory: () => getToken(authCmd),
+        };
+        return init;
+    }
+}
+
